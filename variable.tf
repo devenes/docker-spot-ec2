63a4c0b48318901942ad02b56697679a4867568d
Enes <66560757+devenes@users.noreply.github.com>
2022-06-12 22:49:46 +0300

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "region" {}
+variable "security_group" {}
+variable "key_name" {}
+variable "instance_type" {}
+variable "instance_ami" {}
+variable "ec2_name" {}
+variable "instance_count" {}
+variable "ec2_user" {}
+variable "iam_instance_profile" {}
+variable "profile" {}
+variable "spot_price" {}
